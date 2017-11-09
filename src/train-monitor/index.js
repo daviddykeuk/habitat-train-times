4e045787098db9bd80261c2c5d323d0a468e8224
David Dyke <david.dyke@tnsglobal.com>
2017-11-09 20:40:01 +0000

    fixed some errors

--- src/train-monitor/index.js
+++ src/train-monitor/index.js
@@ -11,31 +11,34 @@ module.exports = function(args) {
         // check that it works
         args.transportApi.getDepartures({ stationCode: args.stationCode }).then((response) => {
 
-            // use the buffer to change status
-            response = bufferProcessor(response);
+            try {
+                // use the buffer to change status
+                response = bufferProcessor(response);
 
-            // add the result in to memory to serve in the API
-            global.departures = response;
+                // add the result in to memory to serve in the API
+                global.departures = response;
 
-            // process the results
-            var processedTrains = trainProcessor(response);
+                // process the results
+                var processedTrains = trainProcessor(response);
 
-            // emit events on the late trains
-            if (processedTrains.newLateTrains.length > 0) {
-                args.eventEmitter.emit('trains.late', processedTrains.newLateTrains);
+                // emit events on the late trains
+                if (processedTrains.newLateTrains.length > 0) {
+                    args.eventEmitter.emit('trains.late', processedTrains.newLateTrains);
+                }
+
+                // emit events on the cancelled trains
+                if (processedTrains.newCancelledTrains.length > 0) {
+                    args.eventEmitter.emit('trains.cancelled', processedTrains.newCancelledTrains);
+                }
+
+                // emit events on the on time trains
+                if (processedTrains.newOntimeTrains.length > 0) {
+                    args.eventEmitter.emit('trains.ontime', processedTrains.newOntimeTrains);
+                }
+                setTimeout(run, 60000 * 2);
+            } catch (e) {
+                setTimeout(run, 60000 * 2);
             }
-
-            // emit events on the cancelled trains
-            if (processedTrains.newCancelledTrains.length > 0) {
-                args.eventEmitter.emit('trains.cancelled', processedTrains.newCancelledTrains);
-            }
-
-            // emit events on the on time trains
-            if (processedTrains.newOntimeTrains.length > 0) {
-                args.eventEmitter.emit('trains.ontime', processedTrains.newOntimeTrains);
-            }
-
-            setTimeout(run, 60000 * 2);
         }).catch((err) => {
             console.log("error %s", err)
             setTimeout(run, 60000 * 2);
